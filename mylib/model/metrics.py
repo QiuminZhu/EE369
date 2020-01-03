76eacefcd08d5e587a792e0d579e2d810a223503
QiuminZhu <58215324+QiuminZhu@users.noreply.github.com>
2020-01-03 10:05:37 +0800

    Delete metrics.py

--- mylib/model/metrics.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from keras import backend as K
-
-def auc(y_true, y_pred):
-    auc = tf.metrics.auc(y_true, y_pred)[1]
-    K.get_session().run(tf.local_variables_initializer())
-    return auc
